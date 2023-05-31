7cb5d24f407736c830f4a7767260ecab40988718
SergioAldana <saaldana2@misena.edu.co>
2023-05-31 11:38:03 -0500

    Creación de la estructura base del proyecto con Spring Initializr

---
+++ src/main/java/com/backend/ejemplo_API_REST/EjemploApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ejemplo_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/ejemplo_API_REST/EjemploApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ejemplo_API_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
