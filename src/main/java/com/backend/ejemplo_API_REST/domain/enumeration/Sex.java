fe89b41dd4baf91be9184ea0bacfadb9cd6bdade
SergioAldana <saaldana2@misena.edu.co>
2023-06-02 16:01:57 -0500

    Enumerados Sex y State creados

---
+++ src/main/java/com/backend/ejemplo_API_REST/domain/enumeration/Sex.java
@@ -0,0 +1,7 @@
+package com.backend.ejemplo_API_REST.domain.enumeration;
+
+public enum Sex {
+
+    MALE, FEMALE
+
+}
---
+++ src/main/java/com/backend/ejemplo_API_REST/domain/enumeration/State.java
@@ -0,0 +1,7 @@
+package com.backend.ejemplo_API_REST.domain.enumeration;
+
+public enum State {
+
+    ACTIVO, INACTIVO
+
+}
