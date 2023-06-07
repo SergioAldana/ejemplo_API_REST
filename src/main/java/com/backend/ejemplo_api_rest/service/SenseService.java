49cf862d3b663264b5d05ba93de488d18905edbe
SergioAldana <saaldana2@misena.edu.co>
2023-06-07 16:42:12 -0500

    Métodos saveSense, findAllSenses, findById y deleteById creados

--- src/main/java/com/backend/ejemplo_api_rest/service/SenseService.java
+++ src/main/java/com/backend/ejemplo_api_rest/service/SenseService.java
@@ -4,22 +4,23 @@ import com.backend.ejemplo_api_rest.domain.Sense;
 import com.backend.ejemplo_api_rest.repository.SenseRepository;
 import com.backend.ejemplo_api_rest.service.dto.SenseDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 @RequiredArgsConstructor
 @Service
 public class SenseService {
 
-    // private final Logger
+    // private final Logger log = LoggerFactory
 
     private final SenseRepository senseRepository;
 
     private final SenseDTO senseDTO;
+
 
     public Sense saveSense(SenseDTO senseDTO) {
         Sense sense = new Sense();
@@ -31,43 +32,26 @@ public class SenseService {
         return senseRepository.save(sense);
     }
 
-    public SenseDTO saveSense(SenseDTO senseDTO) {
-        Sense sense = new Sense();
-
-        sense.setId(senseDTO.getId());
-        sense.setType(senseDTO.getType());
-        sense.setState(senseDTO.getState());
-
-        return senseRepository.save(sense);
-    }
-
     public List<SenseDTO> findAllSenses() {
         List<Sense> senses = senseRepository.findAll();
-        List<SenseDTO> senseDTOs = new ArrayList();
-        SenseDTO senseDTO = new SenseDTO();
+        List<SenseDTO> sensesDTO = new ArrayList<>();
 
         for (Sense sense : senses) {
             senseDTO.setId(sense.getId());
             senseDTO.setType(sense.getType());
             senseDTO.setState(sense.getState());
         }
-
-        return senseDTOs;
+        return sensesDTO;
     }
 
-    /**
-    *   public Sense findById(Long id) {
-    *       return senseRepository.findById(id).orElse(null);
-    *   }
-    */
+    public SenseDTO findById(Long id) {
+        Sense sense = senseRepository.findById(id).orElse(null);
 
-    public Optional<> findById(Long id) {
-        return senseRepository.findById(id);
+        senseDTO.setId(sense.getId());
+        return senseDTO;
     }
 
     public void deleteById(Long id) {
         senseRepository.deleteById(id);
     }
-
-
 }
