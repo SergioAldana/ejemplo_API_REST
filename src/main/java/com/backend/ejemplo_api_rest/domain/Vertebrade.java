984886a85a22768b64e434f5249e8e991691cb1f
SergioAldana <sergio.aldana2000@gmail.com>
2025-10-30 12:14:10 -0500

    Se elimina la clase Vertebrade y el enum Sex

--- src/main/java/com/backend/ejemplo_api_rest/domain/Vertebrade.java
+++
@@ -1,7 +0,0 @@
-package com.backend.ejemplo_api_rest.domain;
-
-public class Vertebrade extends Animal {
-
-
-
-}
--- src/main/java/com/backend/ejemplo_api_rest/domain/enumeration/Sex.java
+++
@@ -1,7 +0,0 @@
-package com.backend.ejemplo_api_rest.domain.enumeration;
-
-public enum Sex {
-
-    MALE, FEMALE
-
-}
