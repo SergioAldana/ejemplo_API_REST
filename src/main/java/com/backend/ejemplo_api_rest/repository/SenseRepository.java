c764458486aa4c3115f4967e74e0af7b6e18ffee
SergioAldana <sergio.aldana2000@gmail.com>
2025-10-30 17:01:46 -0500

    Se elimina la clase SenseRepository

--- src/main/java/com/backend/ejemplo_api_rest/repository/SenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.ejemplo_api_rest.repository;
-
-import com.backend.ejemplo_api_rest.domain.Sense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SenseRepository extends JpaRepository<Sense, Long> {
-}
