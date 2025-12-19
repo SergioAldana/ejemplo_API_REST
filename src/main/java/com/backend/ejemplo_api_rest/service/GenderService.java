9b750e280c00884a0c6c8be07f76cbc7ef696b5c
SergioAldana <sergio.aldana2000@gmail.com>
2025-12-19 14:53:51 -0500

    Modificaciones de los métodos registerGender y mapToEntity, y definición del createGender

--- src/main/java/com/backend/ejemplo_api_rest/service/GenderService.java
+++ src/main/java/com/backend/ejemplo_api_rest/service/GenderService.java
@@ -3,6 +3,7 @@ package com.backend.ejemplo_api_rest.service;
 import com.backend.ejemplo_api_rest.domain.Gender;
 import com.backend.ejemplo_api_rest.repository.GenderRepository;
 import com.backend.ejemplo_api_rest.service.dto.GenderDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,11 +25,29 @@ public class GenderService {
      * el flujo completo.
      */
     public GenderDTO registerGender(GenderDTO genderDTO) {
-        LOG.debug("Información creada para GenderDTO: {}", genderDTO);
-        Gender newGender = mapToEntity(genderDTO);
+        /*
+         * Opción 1:
+         * Creación de la instancia en el método principal.
+         * Luego, envío el DTO y la instancia al método ?
+         */
+        Gender newGender = new Gender();
+        mapToEntity(genderDTO, newGender);
+        /*
+         * Opción 2:
+         * Envío del DTO al método mapToEntity
+         * Gender newGender = mapToEntity(genderDTO);
+         */
+
         Gender savedGender = genderRepository.save(newGender);
-        return mapToDTO(savedGender);
+        GenderDTO newGenderDTO = mapToDTO(savedGender);
+        LOG.debug("Información creada para Género: {}", newGenderDTO);
+        return newGenderDTO;
     }
+
+    /**
+     * Registro para el administrador, sin validaciones, ni seguridad (opcional).
+     * createGender
+     */
 
     /*
      * Conversión desde el DTO ⇾ a la Entidad.
@@ -36,12 +55,11 @@ public class GenderService {
      * Control del dominio
      * Seguridad
      * Trazabilidad
+     * Una única responsabilidad; mapToEntity SOLO transcribe datos.
      */
-    private Gender mapToEntity(GenderDTO DTO) {
-        Gender entity = new Gender();
+    private void mapToEntity(GenderDTO DTO, Gender entity) {
         entity.setGenderIdentity(DTO.getGenderIdentity());
         entity.setState(DTO.getState());
-        return entity;
     }
 
     /*
